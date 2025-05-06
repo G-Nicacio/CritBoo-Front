c3182ab7c81039313bea85b5e6b1e11d9675e31c
GabroSC <gabrielsc5@al.insper.edu.br>
2025-05-06 01:23:14 -0300

    ajustes com integração do back

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -5,6 +5,7 @@ import { Carrossel } from "./components/Carrossel";
 import { Jogos } from './Jogos';
 import Perfil from './pages/Perfil';
 import EditarPerfil from './pages/EditarPerfil';
+import { Jogo } from '../../Jogo';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Carrossel />} />
           <Route path="/jogos" element={<Jogos />} />
+          <Route path='/jogos/:id' element={<Jogo />} />
           <Route path="/perfil" element={<Perfil />} />
           <Route path="/editar-perfil" element={<EditarPerfil />} />
         </Routes>
--- my-app/src/Jogos.jsx
+++ my-app/src/Jogos.jsx
@@ -1,55 +1,66 @@
+import { useEffect, useState } from "react";
+import { DataGrid } from "@mui/x-data-grid";
 import React from 'react';
 import { Grid, Card, CardMedia, Typography, CardContent } from '@mui/material';
 import { Carrossel2 } from './components/Carrossel2';
 import { useNavigate } from 'react-router-dom';
 
-const games = [
-    { id: 1, nome: "God of War Ragnarok", imagem: "/gow-ragnarok.jpg" },
-    { id: 2, nome: "Hollow Knight", imagem: "/hk.jpg" },
-    { id: 3, nome: "Red Dead Redemption II", imagem: "/rdr2.jpg" },
-    { id: 4, nome: "Celeste", imagem: "/celeste.jpg" },
-    { id: 5, nome: "Elden Ring", imagem: "/eldenring.jpg" }
-  ];
-
-  export function Jogos() {
+export function Jogos() {
     const navigate = useNavigate();
+  const [jogos, setJogo] = useState([]);
 
-    return (
-        <>
-        <div className="jogoCarrossel" style={{ paddingTop: '100px' }}></div>
+  useEffect(() => {
+    fetch("http://localhost:8080/jogos", { method: "GET" })
+      .then((response) => {
+        if (!response.ok) throw new Error("Erro ao buscar jogos");
+        return response.json();
+      })
+      .then((data) => {
+        setJogo(data);
+      })
+      .catch((error) => {
+        console.error("Erro ao buscar jogos:", error);
+      });
+  }, []);
 
-        <Typography variant="h4" fontWeight="bold" align="center" gutterBottom>
-            Jogos Populares
-        </Typography>
 
-        <Carrossel2 />
 
-      <Grid
-        container
-        spacing={3}
-        columns={{ xs: 4, sm: 8, md: 12 }}
-        sx={{ padding: 2, paddingTop: 10 }}
-      >
-        {games.map((game, index) => (
-          <Grid item key={index} xs={6} sm={4} md={4}>
-            <Typography onClick={() => navigate(`/jogo/${game.id}`)} style={{ cursor: 'pointer' }} align='center'>
-            <Card sx={{ borderRadius: 3, boxShadow: 4 }} style={{ backgroundColor: 'black', color: 'white' }}>
-              <CardMedia
-                component="img"
-                height="220"
-                image={game.imagem}
-                alt={game.nome}
-                />
-              <CardContent sx={{ textAlign: 'center', py: 2 }}>
-                <Typography variant="h6" fontWeight="bold">
-                  {game.nome}
-                </Typography>
-              </CardContent>
-            </Card>
+  return (
+    <>
+    <div className="jogoCarrossel" style={{ paddingTop: '100px' }}></div>
+
+    <Typography variant="h4" fontWeight="bold" align="center" gutterBottom>
+        Jogos Populares
+    </Typography>
+
+    <Carrossel2 />
+
+  <Grid
+    container
+    spacing={3}
+    columns={{ xs: 4, sm: 8, md: 12 }}
+    sx={{ padding: 2, paddingTop: 10 }}
+  >
+    {jogos.map((game, index) => (
+      <Grid item key={index} xs={6} sm={4} md={4}>
+        <Typography onClick={() => navigate(`/jogos/${game.id}`)} style={{ cursor: 'pointer' }} align='center'>
+        <Card sx={{ borderRadius: 3, boxShadow: 4 }} style={{ backgroundColor: 'black', color: 'white' }}>
+          <CardMedia
+            component="img"
+            height="220"
+            image={game.imagem}
+            alt={game.nomeJogo}
+            />
+          <CardContent sx={{ textAlign: 'center', py: 2 }}>
+            <Typography variant="h6" fontWeight="bold">
+              {game.nomeJogo}
             </Typography>
-          </Grid>
-        ))}
+          </CardContent>
+        </Card>
+        </Typography>
       </Grid>
-      </>
-    );
-  }
+    ))}
+  </Grid>
+  </>
+);
+}
